59387b9752b3d0eb3fa6be797ca1bb0492161d9e
remotecom <techops-team@remote.com>
2025-08-26 10:27:01 +0200

    chore: upgrade patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BEChallengex.MixProject do
   def project do
     [
       app: :be_challengex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
